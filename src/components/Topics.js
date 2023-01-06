010c9a9a11e03e2d8f16e99622156b523a0c1137
yadavyogi <115787274+yadavyogi@users.noreply.github.com>
2023-01-07 00:02:54 +0530

    Update Topics.js

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,19 +1,27 @@
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
 const Topics = () => {
+  const history = useHistory();
   return (
     <div id="topics-div">
       <h2>Topics</h2>
       <ul>
         <li>
-          <h2 id="rendering">Rendering</h2>
+          <Link to="/topics/rendering">
+            <h2 id="rendering">Rendering</h2>
+          </Link>
         </li>
         <li>
-          <h2 id="components">Components</h2>
+          <Link to="/topics/components">
+            <h2 id="components">Components</h2>
+          </Link>
         </li>
       </ul>
       <hr />
-      <button id="topics-back-button">Back</button>
+      <button id="topics-back-button" onClick={() => history.push("/")}>
+        Back
+      </button>
     </div>
   );
 };
