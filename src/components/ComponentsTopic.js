13453b72159129e6442fd7cb083d8ad78583df77
yadavyogi <115787274+yadavyogi@users.noreply.github.com>
2023-01-06 23:58:11 +0530

    Update ComponentsTopic.js

--- src/components/ComponentsTopic.js
+++ src/components/ComponentsTopic.js
@@ -1,10 +1,12 @@
 import React from "react";
+import {useHistory} from "react-router-dom"
 
 const ComponentsTopic = () => {
+  const history = useHistory();
   return (
     <div id="components-div">
       <h3>Components in React</h3>
-      <button id="components-back-button">
+      <button id="components-back-button" onClick={() => history.push("/topics")}>
         Back
       </button>
     </div>
